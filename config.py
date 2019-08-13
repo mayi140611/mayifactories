b4fca59e0bfb131565be257659db99722503d65f
luoyonggui <ygluo@leapstack.cn>
2019-08-13 15:11:16 +0800

    sm

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 LOG_PATH='/Users/luoyonggui/Documents/logs'
-# TS_CODES=['601318.SH','600029.SH','601939.SH']
-TS_CODES=['000001.SZ']
+# STOCK_NAMES=['平安银行']
+STOCK_NAMES=['东方航空', '南方航空', '中国核电', '中国平安', '招商银行', '建设银行', '工商银行', '农业银行', '北大荒']
--- timed_tasks.py
+++ timed_tasks.py
@@ -8,11 +8,12 @@
 
 定时任务
 """
+import pandas as pd
 import logging
 import os
 import sys
 sys.path.append('/Users/luoyonggui/PycharmProjects/mayiutils_n1/mayiutils/config')
-from config import LOG_PATH, TS_CODES
+from config import LOG_PATH, STOCK_NAMES
 
 logger = logging.getLogger(__file__)
 logger.setLevel(level=logging.INFO)
@@ -30,11 +31,12 @@ logger.addHandler(handler)
 stock2mongo.py [-h] [--ts_code TS_CODE] [--mode MODE]
                       [--start_date START_DATE] [--end_date END_DATE]
 """
-for TS_CODE in TS_CODES:
-    logger.info(f'读取数据{TS_CODE}，存入MongoDB')
-    cmd = f"python finance/stock2mongo.py --ts_code {TS_CODE}"
+stock_series = pd.read_pickle('finance/data/stock_dict.pkl')
+for name in STOCK_NAMES:
+    logger.info(f'读取数据{name}，存入MongoDB')
+    cmd = f"python finance/stock2mongo.py --ts_code {stock_series.loc[name]}"
     r = os.system(cmd)
     if r == 0:
-        logger.info(f'读取数据{TS_CODE}，存入MongoDB，complete')
+        logger.info(f'读取数据{name}，存入MongoDB，complete')
     else:
-        logger.error(f'读取数据{TS_CODE}，存入MongoDB，failure')
+        logger.error(f'读取数据{name}，存入MongoDB，failure')
