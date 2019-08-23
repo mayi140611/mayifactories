c4be1d86e6cd7eb28f6b696c8f90e0d3f5e22bdd
luoyonggui <ygluo@leapstack.cn>
2019-08-23 22:11:07 +0800

    sm

--- finance/cal_portfolio_val.py
+++ finance/cal_portfolio_val.py
@@ -37,27 +37,33 @@ logger = get_logger(__file__, file_handler=True, log_path=log_path)
 if __name__ == '__main__':
 
     mongo = PyMongoWrapper()
-    # 获取基金的值
     rlist = []
-    total_val = fund_val = stock_val = 0
-    for i in portfolio['fund']:
-        table = mongo.getCollection('finance', i[0])
-        r = list(mongo.findAll(table, fieldlist=['trade_date', 'close'], sort=[('trade_date', -1)], limit=1))[0]
-        rlist.append((i[0], r['close'], i[1], r['trade_date']))
-        fund_val += r['close'] * i[1]
     stock_series = pd.read_pickle('finance/data/stock_dict.pkl')
-    for i in portfolio['stock']:
-        table = mongo.getCollection('finance', stock_series.loc[i[0]])
-        r = list(mongo.findAll(table, fieldlist=['trade_date', 'close'], sort=[('trade_date', -1)], limit=1))[0]
-        rlist.append((i[0], r['close'], i[1], r['trade_date']))
-        stock_val += r['close'] * i[1]
+    for t in ['fund', 'stock']:
+        for i in portfolio[t]:
+            tname = i[0] if t == 'fund' else stock_series.loc[i[0]]
+            table = mongo.getCollection('finance', tname)
+            r = list(mongo.findAll(table, fieldlist=['trade_date', 'close', 'pct_chg'], sort=[('trade_date', -1)], limit=1))[0]
+            rlist.append((i[0], r['close'], i[1], r['pct_chg'], t, r['trade_date']))
+
+    df = pd.DataFrame(rlist, columns=['code', 'close', 'vol', '涨幅', '类型', 'trade_date'])
+    df['市值'] = df['close'] * df['vol']
+    df['change'] = ((df['close'] - df['close']/(1+df['涨幅']/100)) * df['vol']).map(int)
+
+    def t(i):
+        return f'<font color="red">{i}</font>' if i > 0 else f'<font color="green">{i}</font>'
+    df['trade_date'] = df['trade_date'].dt.date
+    df['市值'] = df['市值'].map(int)
+    df['涨幅'] = df['涨幅'].map(t)
+    df.sort_values('市值', ascending=False, inplace=True)
     logger.info(rlist)
-    total_val = fund_val + stock_val
-    logger.info(f'total_val: {total_val}, fund_val: {fund_val}, stock_val: {stock_val}')
+    total_val = df['市值'].sum()
+    loss = df['change'].sum()
+    logger.info(f'total_val: {total_val}, loss: {loss}')
     # 图片
-    labels = [i[0] for i in rlist]
-    students = [i[1]*i[2] for i in rlist]
-    plt.pie(students, labels=labels, autopct='%3.2f%%', startangle=45, shadow=True,
+    labels = df['code']
+    val = df['市值']
+    plt.pie(val, labels=labels, autopct='%3.2f%%', startangle=45, shadow=True,
             )
     # 设置x，y轴刻度一致，这样饼图才能是圆的
     plt.axis('equal')
@@ -68,16 +74,16 @@ if __name__ == '__main__':
     # 构造文字内容
     rr = '\n'.join([f'{i[0]} {i[1]} {i[2]} {str(i[3])[:10]}' for i in rlist])
     logger.info(rr)
-    text = f'total_val1: {total_val}, fund_val: {fund_val}, stock_val: {stock_val}'
+    text = f'total_val1: {total_val}, loss: {loss}'
     d = html_ops.create_html('report报告222')
 
-    d('body').append('<p>Hello world222!</p><br>')
+    d('body').append('<p>Hello Ian!</p><br>')
     d('body').append('Here is the <a href="http://www.baidu.com">link</a> you wanted.<br>')
-    table = html_ops.create_table(['code', 'close', 'vol', 'trade_date'], rlist)
+    table = html_ops.create_table(df[['code', '涨幅', 'change', '市值', 'close', 'vol', '类型', 'trade_date']])
     d('body').append(table)
     d('body').append('<br>')
     d('body').append(f'{text}<br>')
     img = html_ops.create_img(img_path)
     d('body').append(img)
-
+    # html_ops.output2html(d, 'tmp.html')
     send_email(subject, text, d.outer_html())
