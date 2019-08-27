a5efa5e728357c27670d3409f2d8ba9694866b58
luoyonggui <ygluo@leapstack.cn>
2019-08-27 16:09:34 +0800

    sm

--- finance/stock2mongo.py
+++ finance/stock2mongo.py
@@ -27,7 +27,7 @@ logger = get_logger(__file__, file_handler=True, log_path=log_path)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='BiLSTM-CRF for Chinese NER task')
     parser.add_argument('--ts_code', type=str, help='train data source')
-    parser.add_argument('--mode', type=str, default='stock', help='#stock|index|fund, 默认stock')
+    parser.add_argument('--mode', type=str, default='stock', help='#stock|index|fund|otc_fund, 默认stock')
     parser.add_argument('--start_date', type=str, default='20010101', help='#默认读取股票')
     parser.add_argument('--end_date', type=str, default=datetime.now().strftime('%Y%m%d'), help='#默认读取股票')
     args = parser.parse_args()
@@ -40,16 +40,40 @@ if __name__ == '__main__':
     if args.ts_code not in tables:
         logger.info(f'{args.ts_code}表在数据库中不存在，新建表插入！')
 
-        if args.mode in ['stock', 'index', 'fund']:
+        if args.mode in ['stock', 'index', 'fund', 'otc_fund']:
             mongo.setUniqueIndex('finance', args.ts_code, 'trade_date')
     else:
         logger.info(f'{args.ts_code}表在数据库中存在，update表数据！')
+
         r = mongo.findAll(table, fieldlist=['trade_date'], sort=[('trade_date', -1)], limit=1)
         d = list(r)[0]['trade_date']
         args.start_date = (d + timedelta(days=1)).strftime('%Y%m%d')
-    df = daily(args.ts_code, start_date=args.start_date, end_date=args.end_date, mode=args.mode)
-    logger.info(f'共请求到{df.shape[0]}条数据！')
-    if df.shape[0] > 0:
-        table.insert_many(df2dicts_stock(df))
-    else:
-        logger.warning('请求数据为空！')
+    try:
+        if args.mode in ['stock', 'index', 'fund']:
+            df = daily(args.ts_code, start_date=args.start_date, end_date=args.end_date, mode=args.mode)
+        elif args.mode in ['otc_fund']:
+            import requests
+            import json
+            import pandas as pd
+
+            headers = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36 Maxthon/5.2.1.6000',
+            }
+            r = requests.get(f'http://fundgz.1234567.com.cn/js/{args.ts_code}.js', headers=headers)
+            s = r.text
+            d = json.loads(s[s.find('{'):(s.find('}') + 1)])
+            dd = dict()
+            dd['trade_date'] = datetime.strptime(d['jzrq'], '%Y-%m-%d')
+            dd['close'] = d['dwjz']
+            df = pd.DataFrame(columns=['trade_date', 'close'])
+            df = df.append(dd, ignore_index=True)
+            # print(d, dd, df)
+
+        logger.info(f'共请求到{df.shape[0]}条数据！')
+        if df.shape[0] > 0:
+            mongo.insertDataframe(df, 'finance', args.ts_code)
+            # table.insert_many(df2dicts_stock(df))
+        else:
+            logger.warning('请求数据为空！')
+    except Exception as e:
+        logger.error(str(e))
--- timed_tasks.py
+++ timed_tasks.py
@@ -11,7 +11,7 @@
 import pandas as pd
 import os
 
-from config import LOG_PATH, STOCK_NAMES, FUND_NAMES, INDEX_NAMES
+from config import LOG_PATH, STOCK_NAMES, FUND_NAMES, INDEX_NAMES, OTC_FUND_NAMES
 import sys
 # print(sys.path)
 sys.path.append('/Users/luoyonggui/PycharmProjects/mayiutils_n1/mayiutils/config')
@@ -49,7 +49,15 @@ stock2mongo.py [-h] [--ts_code TS_CODE] [--mode MODE]
 #         logger.info(f'读取数据{name}，存入MongoDB，complete')
 #     else:
 #         logger.error(f'读取数据{name}，存入MongoDB，failure')
-
+fund_otc_series = pd.read_pickle('finance/data/fund_otc_series.pkl')
+for name in OTC_FUND_NAMES:
+    logger.info(f'读取数据{name}，存入MongoDB')
+    cmd = f"python finance/stock2mongo.py --ts_code {fund_otc_series.loc[name]} --mode otc_fund"
+    r = os.system(cmd)
+    if r == 0:
+        logger.info(f'读取数据{name}，存入MongoDB，complete')
+    else:
+        logger.error(f'读取数据{name}，存入MongoDB，failure')
 # ---------------------
-cmd = f"python finance/cal_portfolio_val.py"
-r = os.system(cmd)
+# cmd = f"python finance/cal_portfolio_val.py"
+# r = os.system(cmd)
