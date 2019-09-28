94c3005b2a03949ca613941b47e065ee88837ac4
luoyonggui <ygluo@leapstack.cn>
2019-09-28 21:31:07 +0800

    sm

--- finance/plate_perspective.py
+++ finance/plate_perspective.py
@@ -8,32 +8,97 @@
 """
 import pandas as pd
 from datetime import datetime, timedelta
+import os
 import sys
 sys.path.append('/Users/luoyonggui/PycharmProjects/mayiutils_n1/mayiutils/db')
 from pymongo_wrapper import PyMongoWrapper
+sys.path.append('/Users/luoyonggui/PycharmProjects/mayiutils_n1/mayiutils')
+from email_ops import send_email
+import html_ops
 
-# plate = '银行'
-# plate = '证券'
-plate = '白酒'
+PROJ_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+stock_series = pd.read_pickle(os.path.join(PROJ_DIR, 'finance/data/stock_dict.pkl'))
+fund_otc_series = pd.read_pickle(os.path.join(PROJ_DIR, 'finance/data/fund_otc_series.pkl'))
+mongo = PyMongoWrapper()
 time_range = 'near_1_year'
 start_date = datetime.now() - timedelta(days=365)
-stock_series = pd.read_pickle('finance/data/stock_dict.pkl')
-fund_otc_series = pd.read_pickle('finance/data/fund_otc_series.pkl')
-mongo = PyMongoWrapper()
-# 获取该板块的所有股票
-table = mongo.getCollection('finance', 'stock_basic')
-dfr = mongo.findAll(table, {'industry': plate}, returnFmt='df')
-print(dfr.shape)
-print(dfr.head())
-# 下载股票行情
+stock_pool = [
+    # '东方航空',
+    # '南方航空',
+    # '白云机场',
+    # '中国核电',
+    # '山鹰纸业',
+    # '晨鸣纸业',
+    # '中信证券',
+    # '华泰证券',
+    # '国元证券',
 
-# 从mongo中load相应的close, pct_chg
+    # '兴业银行',
+    # '招商银行',
+    # '平安银行',
+
+    # '中国平安',
+    # '中国人寿',
+    # '中国太保',
+
+    # '贵州茅台',
+    # '五粮液',
+    # '泸州老窖',
+    '格力电器',
+    '美的集团',
+    '福耀玻璃',
+    '恒瑞医药',
+]
+
+# # plate = '银行'
+# # plate = '证券'
+# plate = '白酒'
+# # 获取该板块的所有股票
+# table = mongo.getCollection('finance', 'stock_basic')
+# dfr = mongo.findAll(table, {'industry': plate}, returnFmt='df')
+# print(dfr.shape)
+# print(dfr.head())
+
+
+dfr = pd.DataFrame(stock_pool, columns=['name'])
+dfr['ts_code'] = dfr.name.map(lambda s: stock_series.loc[s])
+# 基本面 area、industry、sw_industry、市值、市值排名
+table = mongo.getCollection('finance', 'stock_basic')
+df_base = mongo.findAll(table, {'ts_code': {'$in': dfr.ts_code.tolist()}}, fieldlist=['name', 'area', 'industry', 'market'],
+                   returnFmt='df')
+# print(df)
+table = mongo.getCollection('finance', 'swclass')
+dft = mongo.findAll(table, {'股票代码': {'$in': dfr.ts_code.map(lambda s: s.split('.')[0]).tolist()}},
+                   fieldlist=['股票名称', '行业名称'],
+                   returnFmt='df')
+dft.columns = ['name', 'swclass']
+df_base = pd.merge(df_base, dft, on='name')
+
+
+table = mongo.getCollection('finance', 'stock_daily_basic1')
+dft = mongo.findAll(table, {'$and': [{'trade_date': '20190926'}, {'ts_code': {'$in': dfr.ts_code.tolist()}}]},
+                   fieldlist=['ts_code', '市盈率（总市值/净利润）', '市净率（总市值/净资产）', '总市值', '流通市值（万元）', 'rank'],
+                   returnFmt='df')
+dft = pd.merge(dft, dfr, on='ts_code')
+df_base = pd.merge(df_base, dft, on='name')
+df_base.set_index('name', inplace=True)
+df_base = df_base.T.reset_index()
+print()
+
+# 行情
 # 基准
 table = mongo.getCollection('finance', '000001.SH')
 df = mongo.findAll(table, {'trade_date': {'$gte': start_date}}, fieldlist=['trade_date', 'pct_chg'],
                     sort=[('trade_date', 1)], returnFmt='df')
 for i in dfr.itertuples():
+    # 下载股票行情
+    cmd = f"python {PROJ_DIR}/finance/stock2mongo.py --ts_code {i.ts_code}"
+    r = os.system(cmd)
     table = mongo.getCollection('finance', i.ts_code)
+
     dft = mongo.findAll(table, {'trade_date': {'$gte': start_date}}, fieldlist=['trade_date', 'pct_chg'], returnFmt='df')
     df = pd.merge(df, dft, on='trade_date', how='left')
 
@@ -51,8 +116,9 @@ df.columns = ['000001.SH'] + dfr.name.tolist()
 # print(df1y)
 # 近1周涨幅
 df1w = pd.DataFrame(((df.iloc[-1] / df.iloc[-6]) - 1).sort_values(ascending=False))
-df1w.reset_index(inplace=True)
-df1w.columns = ['index_1w', '涨幅_1w']
+# print(df1w)
+# df1w.reset_index(inplace=True)
+
 # 近1月涨幅
 df1m = pd.DataFrame(((df.iloc[-1] / df.iloc[-21]) - 1).sort_values(ascending=False))
 # 近3月涨幅
@@ -60,24 +126,53 @@ df3m = pd.DataFrame(((df.iloc[-1] / df.iloc[-63]) - 1).sort_values(ascending=Fal
 # 近6月涨幅
 df6m = pd.DataFrame(((df.iloc[-1] / df.iloc[-126]) - 1).sort_values(ascending=False))
 # 近1年涨幅排序
-df1y = pd.DataFrame((df.iloc[-1]-1).sort_values(ascending=False))
-df1w['index_1m'] = df1m.index.tolist()
-df1w['涨幅_1m'] = df1m.iloc[:, 0].values.tolist()
-df1w['index_3m'] = df3m.index.tolist()
-df1w['涨幅_3m'] = df3m.iloc[:, 0].values.tolist()
-df1w['index_6m'] = df6m.index.tolist()
-df1w['涨幅_6m'] = df6m.iloc[:, 0].values.tolist()
-df1w['index_1y'] = df1y.index.tolist()
-df1w['涨幅_1y'] = df1y.iloc[:, 0].values.tolist()
+df1y = pd.DataFrame((df.iloc[-1]-1))
+df_mean = pd.DataFrame(df.mean()-1)
+df_std = pd.DataFrame(df.std())
+df1w = pd.concat([df1w, df1m, df3m, df1y, df_mean, df_std], axis=1, sort=True)
+df1w.columns = ['1周涨幅', '1月涨幅', '3月涨幅', '1年涨幅', '平均涨幅', 'std']
+df1w = df1w.applymap(lambda e: round(e, 2))
+df1w = df1w.T
+df1w = df1w.reset_index()
+
 print(df1w)
+# df1w['index_1m'] = df1m.index.tolist()
+# df1w['涨幅_1m'] = list(map(lambda v: round(v, 2), df1m.iloc[:, 0].values.tolist()))
+# df1w['index_3m'] = df3m.index.tolist()
+# df1w['涨幅_3m'] = list(map(lambda v: round(v, 2), df3m.iloc[:, 0].values.tolist()))
+# df1w['index_6m'] = df6m.index.tolist()
+# df1w['涨幅_6m'] = list(map(lambda v: round(v, 2), df6m.iloc[:, 0].values.tolist()))
+# df1w['index_1y'] = df1y.index.tolist()
+# df1w['涨幅_1y'] = list(map(lambda v: round(v, 2), df1y.iloc[:, 0].values.tolist()))
+# print(df1w)
+d = html_ops.create_html(f'report报告')
+t1 = html_ops.create_label('h2', f'基本面')
+d('body').append(t1)
+table = html_ops.create_table(df_base)
+d('body').append(table)
+t1 = html_ops.create_label('h2', f'行情')
+d('body').append(t1)
+table = html_ops.create_table(df1w)
+d('body').append(table)
+d('body').append('<br>')
 
 # 绘制近n日走势图
 import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
 plt.rcParams['axes.unicode_minus'] = False
-df.plot()
-plt.show()
+df.plot(title='近1年涨幅', figsize=(14, 6))
+img_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'a.png')
+plt.savefig(img_path)
+img = html_ops.create_img(img_path)
+d('body').append(img)
+d('body').append('<br>')
+# plt.show()
 df3m = df.iloc[-63:]/df.iloc[-63]
-df3m.plot()
-plt.show()
+df3m.plot(title='近3月涨幅', figsize=(14, 6))
+plt.savefig(img_path)
+img = html_ops.create_img(img_path)
+d('body').append(img)
+d('body').append('<br>')
 
+
+html_ops.output2html(d, 'tmp.html')
--- finance/stock_daily_basic2mongo.py
+++ finance/stock_daily_basic2mongo.py
@@ -25,32 +25,37 @@ logger = get_logger(__file__, file_handler=True, log_path=log_path)
 
 mongo = PyMongoWrapper()
 dbname = 'finance'
-table_name = 'stock_daily_basic'
+table_name = 'stock_daily_basic1'
 table = mongo.getCollection(dbname, table_name)
 if not mongo.isExists(dbname, table_name):
     mongo.setUniqueIndex(dbname, table_name, ['ts_code', 'trade_date'])
 pro = get_tushare_pro()
 f = 'ts_code,trade_date,close,turnover_rate,turnover_rate_f,volume_ratio,pe,pe_ttm,pb,ps,ps_ttm,total_share,float_share,free_share,total_mv,circ_mv'
-df = pro.daily_basic(ts_code='', trade_date=datetime.now().strftime('%Y%m%d'), fields=f)
+# df = pro.daily_basic(ts_code='', trade_date=datetime.now().strftime('%Y%m%d'), fields=f)
+df = pro.daily_basic(ts_code='', trade_date='20190926', fields=f)
 # df = pro.daily_basic(ts_code='', trade_date='20190827', fields=f)
 if not df.empty:
     logger.info(f'请求到{len(df)}条数据！')
-    df.columns = ['ts_code',
-     'trade_date',
-     'close',
-     '换手率（%）',
-     '换手率（自由流通股）',
-     '量比',
-     '市盈率（总市值/净利润）',
-     '市盈率（TTM）',
-     '市净率（总市值/净资产）',
-     '市销率',
-     '市销率（TTM）',
-     '总股本',
-     '流通股本',
-     '自由流通股本',
-     '总市值',
-     '流通市值（万元）']
+    df.columns = [
+        'ts_code',
+         'trade_date',
+         'close',
+         '换手率（%）',
+         '换手率（自由流通股）',
+         '量比',
+         '市盈率（总市值/净利润）',
+         '市盈率（TTM）',
+         '市净率（总市值/净资产）',
+         '市销率',
+         '市销率（TTM）',
+         '总股本',
+         '流通股本',
+         '自由流通股本',
+         '总市值',
+         '流通市值（万元）'
+    ]
+    df = df.sort_values('总市值', ascending=False)
+    df['rank'] = range(1, df.shape[0]+1)
     print(df.head())
     try:
         mongo.insertDataframe(df, dbname, table_name)
