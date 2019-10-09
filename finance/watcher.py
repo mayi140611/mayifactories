91f15269685937af71b7b062c5dc3bcce76c2848
luoyonggui <ygluo@leapstack.cn>
2019-10-09 08:37:37 +0800

    sm

---
+++ finance/watcher.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+"""
+@author: Ian
+@file: watcher.py
+@time: 2019-10-09 08:03
+"""
+import requests
+import sys
+sys.path.append('/Users/luoyonggui/PycharmProjects/mayiutils_n1/mayiutils')
+from email_ops import send_email
+
+
+targets=[
+    ('sz000063', 31.77, 'lte'),
+    ('sh600446', 20.92, 'lte'),
+]
+
+url = 'http://hq.sinajs.cn/list='
+
+for t in targets:
+    url += t[0] + ','
+
+r = requests.get(url)
+# print(r.text)
+
+rs = r.text.split(';\n')
+rr = []
+for r in rs:
+    if r:
+        s = r.find('"')
+        r1 = r[s + 1: -2].split(',')
+        rr.append([r1[0], r1[3]])
+    # print(r1)
+# print(rr)
+rs = []
+for i in range(len(targets)):
+    if float(rr[i][1]) <= targets[i][1]:
+        rs.append(rr[i])
+
+if rs:
+    send_email('watcher', str(rr))
--- timed.sh
+++ timed.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-python timed_tasks.py
+#python timed_tasks.py
 # 计算资产价格
-python finance/cal_portfolio_val.py
+#python finance/cal_portfolio_val.py
 # 爬取每日指标信息
-python finance/stock_daily_basic2mongo.py
+#python finance/stock_daily_basic2mongo.py
+python finance/watcher.py
 
 
